f2f2691c31b16ad04db6b327fa8dd713457eef90
Shivil Raj <shivil.raj@gmail.com>
2016-04-20 19:30:56 +0530

    override as_json in responder model

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -3,4 +3,13 @@ class Responder < ActiveRecord::Base
   validates :capacity, presence: true, inclusion: 1..5
   validates :name, presence: true, uniqueness: true
   validates :type, presence: true
+
+  def as_json(_options = {})
+    super(only:
+            [:emergency_code,
+             :type,
+             :name,
+             :capacity,
+             :on_duty])
+  end
 end
