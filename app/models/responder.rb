55be10ef3c3c3189e127aac80f90fcc6db93a3f8
Shivil Raj <shivil.raj@gmail.com>
2016-04-20 19:14:16 +0530

    update responder controller class

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,5 +1,6 @@
 class Responder < ActiveRecord::Base
   self.inheritance_column = :_type_disabled
-  validates :capacity, :presence => true, :inclusion => 1..5
-  validates :name, :presence => true, :uniqueness => true
+  validates :capacity, presence: true, inclusion: 1..5
+  validates :name, presence: true, uniqueness: true
+  validates :type, presence: true
 end
